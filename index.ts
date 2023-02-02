0fa07f723ea8f75ccb80dbd47b5cea5c8718e706
yuki yamagata <yshanxing4@gmail.com>
2023-02-03 07:16:16 +0900

    first commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+let hasValue = false;
+let count = 10;
+let float= 3.14;
+let negative = -0.12;
+let single = 'hello';
+let double = "hello";
+let back = `hello`;
+
+let hello:string;
+hello = "hello";
---
+++ section01.md
@@ -0,0 +1,7 @@
+# TypeScriptとは
+1. JavaScriptにコンパイルされる
+2. 静的型システムがついた
+3. JavaScriptの上位集合
+
+# TypeScriptが必要な3つの理由
+
